6b67472e61edc37c71b5d35b26bff94353835149
mb62 <git@mb62.de>
2015-06-18 14:27:08 +0200

    Using Play Services for location

--- app/src/main/java/de/mb62/kompass/MainActivity.java
+++ app/src/main/java/de/mb62/kompass/MainActivity.java
@@ -1,42 +1,112 @@
 package de.mb62.kompass;
 
-import android.app.*;
-import android.os.*;
-import android.view.*;
+import android.app.Activity;
+import android.content.Context;
+import android.location.*;
+import android.os.Bundle;
 import android.widget.*;
 import android.hardware.*;
 import android.view.animation.*;
+import android.util.Log;
 
-public class MainActivity extends Activity implements SensorEventListener
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends Activity implements SensorEventListener, ConnectionCallbacks, OnConnectionFailedListener, LocationListener
 {
+
+    // LogCat tag
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+
+    private Location mLastLocation;
+
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+
+    // boolean flag to toggle periodic location updates
+    private boolean mRequestingLocationUpdates = false;
+
+    private LocationRequest mLocationRequest;
+
+    // Location updates intervals in sec
+    private static int UPDATE_INTERVAL = 10000; // 10 sec
+    private static int FATEST_INTERVAL = 5000; // 5 sec
+    private static int DISPLACEMENT = 10; // 10 meters
+
     /** Called when the activity is first created. */
-    private TextView txtDegrees;
+    private TextView latitudeField, longitudeField, txtDegrees;
     private ImageView imgCompass;
     private float currentDegree=0f;
     private SensorManager sensorManager;
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         imgCompass=(ImageView)findViewById(R.id.imgCompass);
         txtDegrees=(TextView)findViewById(R.id.txtDegrees);
         sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+        latitudeField=(TextView)findViewById(R.id.textView2);
+        longitudeField=(TextView)findViewById(R.id.textView4);
+
+        // First we need to check availability of play services
+        if (checkPlayServices()) {
+
+            // Building the GoogleApi client
+            buildGoogleApiClient();
+
+            createLocationRequest();
+        }
+
+        displayLocation();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
     }
 
     @Override
     protected void onResume()
     {
-        // TODO: Implement this method
+        // TODO
         super.onResume();
-        sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_GAME);
+        checkPlayServices();
+        // Resuming the periodic location updates
+        if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onStop() {
+    super.onStop();
+    if (mGoogleApiClient.isConnected()) {
+        mGoogleApiClient.disconnect();
+        }
     }
 
     @Override
     protected void onPause()
     {
-        // TODO: Implement this method
+        // TODO
         super.onPause();
+        stopLocationUpdates();
         sensorManager.unregisterListener(this);
     }
 
@@ -44,7 +114,7 @@ public class MainActivity extends Activity implements SensorEventListener
     public void onSensorChanged(SensorEvent event)
     {
         float degree=Math.round(event.values[0]);
-        txtDegrees.setText("Rotation: "+Float.toString(degree)+" degrees");
+        txtDegrees.setText(Float.toString(degree)+" °");
         RotateAnimation ra=new RotateAnimation(currentDegree,-degree,Animation.RELATIVE_TO_SELF,0.5f,
                 Animation.RELATIVE_TO_SELF,0.5f);
         ra.setDuration(120);
@@ -56,6 +126,124 @@ public class MainActivity extends Activity implements SensorEventListener
     @Override
     public void onAccuracyChanged(Sensor p1, int p2)
     {
-        // TODO: Implement this method
+        // TODO
+    }
+
+    /**
+     * Method to display the location on UI
+     * */
+    private void displayLocation() {
+        mLastLocation = LocationServices.FusedLocationApi
+            .getLastLocation(mGoogleApiClient);
+
+    if (mLastLocation != null) {
+        double latitude = mLastLocation.getLatitude();
+        double longitude = mLastLocation.getLongitude();
+
+        latitudeField.setText(String.valueOf(latitude));
+        longitudeField.setText(String.valueOf(longitude));
+
+    } else {
+        latitudeField.setText("Location disabled");
+        longitudeField.setText("Location disabled");
+        }
+    }
+
+
+    /**
+     * Creating google api client object
+     * */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    /**
+     * Creating location request object
+     * */
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+    }
+
+    /**
+     * Method to verify google play services on the device
+     * */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Starting the location updates
+     * */
+    protected void startLocationUpdates() {
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    /**
+     * Stopping location updates
+     */
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    /**
+     * Google api callback methods
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
+                + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+
+        // Once connected with google api, get the location
+        displayLocation();
+
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Assign the new location
+        mLastLocation = location;
+
+        Toast.makeText(getApplicationContext(), "Location changed!",
+                Toast.LENGTH_SHORT).show();
+
+        // Displaying the new location on UI
+        displayLocation();
     }
 }
